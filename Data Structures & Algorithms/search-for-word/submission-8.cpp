997954020e5d65f48e4e7a45f3e75b7ad97c13ed
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-04-25 02:00:39 +0200

    Add: search-for-word - submission-8

---
+++ Data Structures & Algorithms/search-for-word/submission-8.cpp
@@ -0,0 +1,40 @@
+class Solution {
+private:
+    int ROWS; 
+    int COLS;
+
+    bool backtrack(vector<vector<char>>& board, string word, int r, int c, int i){
+        if(i == word.size()){
+            return true;
+        } 
+
+        if(r < 0 || c < 0 || r >= ROWS || c >= COLS) return false;
+        if(board[r][c] != word[i]) return false;
+        if(board[r][c] == '#') return false;
+
+        board[r][c] = '#';
+
+        bool res = backtrack(board, word, r + 1, c, i + 1) ||
+                   backtrack(board, word, r - 1, c, i + 1) ||
+                   backtrack(board, word, r, c + 1, i + 1) ||
+                   backtrack(board, word, r, c - 1, i + 1);
+        
+        board[r][c] = word[i];
+
+        return res;
+    }
+
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        ROWS = board.size();
+        COLS = board[0].size();
+
+        for(int r = 0; r < ROWS; ++r){
+            for(int c = 0; c < COLS; c++){
+                if(backtrack(board, word, r, c, 0)) return true;
+            }
+        }
+
+        return false;
+    }
+};
