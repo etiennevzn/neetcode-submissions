774d0b0f71b554bddfc59fb89f00e2dc2f61998b
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-04-26 23:58:00 +0200

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.cpp
@@ -0,0 +1,51 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if(!node) return nullptr;
+        unordered_map<Node*, Node*> nodesList;
+
+        queue<Node*> q;
+        q.push(node);
+
+        while(!q.empty()){
+            Node* cur = q.front();
+            q.pop();
+            if(!nodesList.count(cur)){
+                Node* clone = new Node(cur->val);
+                nodesList[cur] = clone;
+            }
+
+            for(Node* neighbor : cur->neighbors){
+                if(!nodesList.count(neighbor)){
+                    q.push(neighbor);
+                    Node* cloneN = new Node(neighbor->val);
+                    nodesList[neighbor] = cloneN;
+                }
+                nodesList[cur]->neighbors.push_back(nodesList[neighbor]);
+            }
+        }
+
+        return nodesList[node];
+    }
+};
