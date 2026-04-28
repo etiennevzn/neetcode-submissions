9da12c980602a1ab7d50b1ee084689a378056dae
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-04-28 14:36:55 +0200

    Add: course-schedule - submission-5

---
+++ Data Structures & Algorithms/course-schedule/submission-5.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    bool bfs(int numCourse, vector<vector<int>>& prerequisites, unordered_set<int>& path){
+        if(path.count(numCourse)) return false;
+
+        path.insert(numCourse);
+        for(vector<int>& n : prerequisites){
+            if(n[0] == numCourse){
+                if(!bfs(n[1], prerequisites, path)) return false;
+                n[0] = -1;
+            }
+        }
+        path.erase(numCourse);
+        return true;
+    }
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        for(int i = 0; i < numCourses; ++i){
+            unordered_set<int> pathNode;
+            if(!bfs(i, prerequisites, pathNode)) return false;
+        }
+        return true;
+    }
+};
