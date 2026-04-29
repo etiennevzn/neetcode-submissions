7dc63689b93d551cad61242a04c2d415615f21f4
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-04-29 22:12:07 +0200

    Add: valid-tree - submission-5

---
+++ Data Structures & Algorithms/valid-tree/submission-5.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+        if(edges.size() > n - 1) return false;
+
+        vector<vector<int>> graph(n);
+        for(const auto& edge : edges){
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+
+        unordered_set<int> visited;
+        queue<pair<int,int>> q;
+        q.push({0,-1});
+
+        while(!q.empty()){
+            auto cur = q.front();
+            q.pop();
+            int node = cur.first, parent = cur.second;
+
+            if(visited.count(node)) return false;
+            visited.insert(node);
+            for(int nei : graph[node]){
+                if(nei != parent) q.push({nei,node});
+            }
+        }
+
+        return visited.size() == n;
+    }
+};
