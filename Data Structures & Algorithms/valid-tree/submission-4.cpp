39af03c32ba6363aebea1fbc0daa01e47fc1165c
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-04-29 21:57:00 +0200

    Add: valid-tree - submission-4

---
+++ Data Structures & Algorithms/valid-tree/submission-4.cpp
@@ -0,0 +1,27 @@
+class Solution {
+private:
+    bool dfs(int node, int parent, vector<vector<int>>& graph, unordered_set<int>& visited){
+        if(visited.count(node)) return false;
+
+        visited.insert(node);
+        for(int nei : graph[node]){
+            if(nei != parent){
+                if(!dfs(nei, node, graph, visited)) return false;
+            }
+        }
+
+        return true;
+    }
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+
+        vector<vector<int>> graph(n);
+        for(const auto& edge : edges){
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+
+        unordered_set<int> visited;
+        return dfs(0, -1, graph, visited) && visited.size() == n;
+    }
+};
