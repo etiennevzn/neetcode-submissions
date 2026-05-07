21857f31483a5e630095427c9678165c2397d9c4
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-05-07 15:59:45 +0200

    Add: decode-ways - submission-0

---
+++ Data Structures & Algorithms/decode-ways/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private:
+    vector<int> mem;
+    int dfs(const string& s, int i){
+        if(i == s.size()) return 1;
+        if(s[i] == '0') return 0;
+        if(mem[i] != -1) return mem[i];
+        
+        string two = "";
+        two += s[i];
+        two += s[i+1];
+        if(i < s.size()-1 && stoi(two) <= 26){
+            mem[i] = dfs(s, i+1) + dfs(s, i+2);
+        }else{
+            mem[i] = dfs(s, i+1);
+        }
+
+        return mem[i];
+    }
+public:
+    int numDecodings(string s) {
+        if(s[0] == '0') return 0; //necessaire ?
+        mem.resize(s.size(), -1);
+        int res = 0;
+        res = dfs(s, 0);
+
+        return res;
+    }
+};
