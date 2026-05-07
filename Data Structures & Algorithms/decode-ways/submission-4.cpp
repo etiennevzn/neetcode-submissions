313c47ebb7d1bb569f22d512d3184e0a2c0aadaf
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-05-07 16:29:17 +0200

    Add: decode-ways - submission-4

---
+++ Data Structures & Algorithms/decode-ways/submission-4.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        vector<int> dp(s.size()+1,0);
+        dp[s.size()] = 1;
+
+        for(int i = s.size()-1; i >= 0; --i){
+            if(s[i] == '0'){
+                dp[i] = 0;
+            }else{
+                dp[i] = dp[i+1];
+                string two = "";
+                two += s[i];
+                two += s[i+1];
+                if(i < s.size()-1 && stoi(two) <= 26) dp[i] += dp[i+2];
+            }
+        }
+        
+        return dp[0];
+    }
+};
