0eed4abff8af870dafcfbc6d9586eb330914a026
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-05-08 23:21:03 +0200

    Add: word-break - submission-9

---
+++ Data Structures & Algorithms/word-break/submission-9.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<bool> dp(s.size()+1, false);
+        dp[s.size()] = true;
+        for(int i = s.size()-1; i >= 0; --i){
+            for(const string& word : wordDict){
+                if(s.substr(i, word.size()) == word) dp[i] = dp[i + word.size()];
+                if(dp[i]) break;
+            }
+        }
+        return dp[0];
+    }
+};
