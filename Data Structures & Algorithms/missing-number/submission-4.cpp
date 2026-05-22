2ce85128886160731f6138bb1c4353ec6d43f232
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-05-22 22:41:03 +0200

    Add: missing-number - submission-4

---
+++ Data Structures & Algorithms/missing-number/submission-4.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        unordered_set<int> s(nums.begin(), nums.end());
+        for(int i = 0; i <= nums.size(); ++i){
+            if(!s.count(i)) return i;
+        } 
+    }
+};
