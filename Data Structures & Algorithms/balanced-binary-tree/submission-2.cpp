36b55ec5c0a2b78458fe0a214119275a758898ad
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-07-05 23:46:32 +0200

    Add: balanced-binary-tree - submission-2

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-2.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if(!root) return true;
+
+        unordered_map<TreeNode*, pair<int,bool>> mp;
+        stack<TreeNode*> s;
+        mp[nullptr] = {0, true};
+        s.push(root);
+
+        while(!s.empty()){
+            TreeNode* cur = s.top();
+            if(cur->left && !mp.count(cur->left)){
+                s.push(cur->left);
+            }else if(cur->right && !mp.count(cur->right)){
+                s.push(cur->right);
+            }else{
+                s.pop();
+                auto [rightHeight, rightBalanced] = mp[cur->right];
+                auto [leftHeight, leftBalanced] = mp[cur->left];
+
+                int height = 1 + max(rightHeight, leftHeight);
+                int balanced = ((rightBalanced && leftBalanced) && abs(rightHeight - leftHeight) <= 1);
+
+                mp[cur] = {height, balanced};
+            }
+        }
+
+        return mp[root].second;
+    }
+};
