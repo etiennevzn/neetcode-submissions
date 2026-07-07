8714e3f10c24607541d709b92d4e5c72950e7b42
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-07-08 01:32:12 +0200

    Add: combination-target-sum - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum/submission-3.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    vector<vector<int>> res;
+    void backtrack(int start, int curSum, vector<int>& cur, vector<int>& nums, int target){
+        if(curSum == target){
+            res.push_back(cur);
+            return;
+        }
+
+        for(int i = start; i < nums.size(); ++i){
+            if(curSum + nums[i] > target) break;
+            cur.push_back(nums[i]);
+            backtrack(i, curSum + nums[i], cur, nums, target);
+            cur.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        res.clear();
+        vector<int> solutions = {};
+        sort(nums.begin(), nums.end());
+        backtrack(0, 0, solutions, nums, target);
+        return res;
+    }
+};
