cf37bc842c184913284353db575c287e8e7f38ae
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-07-08 15:51:41 +0200

    Add: generate-parentheses - submission-2

---
+++ Data Structures & Algorithms/generate-parentheses/submission-2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private:
+    vector<string> res;
+
+    void backtrack(string& cur, int openLeft, int closeLeft, int n){
+        if(!openLeft && !closeLeft){
+            res.push_back(cur);
+            return;
+        }
+
+        if(openLeft){
+            cur.push_back('(');
+            backtrack(cur, openLeft-1, closeLeft, n);
+            cur.pop_back();
+        }
+        if(closeLeft > openLeft){
+            cur.push_back(')');
+            backtrack(cur, openLeft, closeLeft-1, n);
+            cur.pop_back();
+        }
+    }
+public:
+    vector<string> generateParenthesis(int n) {
+        res.clear();
+        string cur = "";
+        backtrack(cur, n, n, n);
+        return res;
+    }
+};
