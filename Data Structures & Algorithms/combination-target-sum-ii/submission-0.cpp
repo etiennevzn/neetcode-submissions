1df4abfe0d87353724ecdb9cc30ea8b3d5edddf3
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-07-08 12:19:11 +0200

    Add: combination-target-sum-ii - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    vector<vector<int>> res;
+
+    void backtrack(int start, int curSum, vector<int>& cur, vector<int>& candidates, int target){
+        if(curSum == target){
+            res.push_back(cur);
+            return;
+        }
+
+        for(int i = start; i < candidates.size(); ++i){
+            if(i > start && candidates[i] == candidates[i-1]) continue;
+            if(curSum + candidates[i] <= target){
+                cur.push_back(candidates[i]);
+                backtrack(i+1, curSum + candidates[i], cur, candidates, target);
+                cur.pop_back();
+            }
+        }
+    }
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        res.clear();
+        sort(candidates.begin(), candidates.end());
+        vector<int> cur;
+        backtrack(0,0,cur,candidates,target);
+        return res;
+    }
+};
