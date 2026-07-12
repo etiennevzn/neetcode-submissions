3f9f9b70e15dcf53933137c025560a9caff0e92a
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-07-13 01:49:15 +0200

    Add: word-ladder - submission-0

---
+++ Data Structures & Algorithms/word-ladder/submission-0.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        if(endWord.empty() || find(wordList.begin(), wordList.end(), endWord) == wordList.end()) return 0;
+
+        unordered_map<string, vector<string>> patterns;
+        wordList.push_back(beginWord);
+
+        for(const auto& word : wordList){
+            for(int j = 0; j < word.size(); ++j){
+                string pattern = word.substr(0, j) + "*" + word.substr(j+1);
+                patterns[pattern].push_back(word);
+            }
+        }
+
+        queue<string> q;
+        q.push(beginWord);
+        int res = 1;
+        unordered_set<string> visited;
+
+        while(!q.empty()){
+            int qLen = q.size();
+            for(int i = 0; i < qLen; ++i){
+                string cur = q.front();
+                q.pop();
+                if(cur == endWord) return res;
+                if(visited.count(cur)) continue;
+
+                visited.insert(cur);
+                for(int j = 0; j < cur.size(); ++j){
+                    string pattern = cur.substr(0, j) + "*" + cur.substr(j+1);
+                    for(const auto& word : patterns[pattern]){
+                        q.push(word);
+                    }
+                }
+            }
+            res++;
+        }
+
+        return 0;
+    }
+};
