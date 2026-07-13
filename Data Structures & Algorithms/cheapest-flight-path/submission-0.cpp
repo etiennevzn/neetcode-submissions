79211bd6906c7d46adf686f7fa0c45be580b5506
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-07-14 00:01:00 +0200

    Add: cheapest-flight-path - submission-0

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<int> prices(n, INT_MAX);
+        prices[src] = 0;
+
+        for(int i = 0; i < k+1; ++i){
+            vector<int> tempPrices = prices;
+            for(const auto& flight : flights){
+                int s = flight[0], d = flight[1], price = flight[2];
+                if(prices[s] != INT_MAX && tempPrices[d] > prices[s] + price) tempPrices[d] = prices[s] + price;
+            }
+            prices = tempPrices;
+        }
+
+        int res = prices[dst];
+        return  res == INT_MAX ? -1 : res;
+    }
+};
