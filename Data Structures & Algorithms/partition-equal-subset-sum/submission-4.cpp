9e116cb6e5e1c7aa50b6f4c660b65ce160fb02e5
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-07-14 15:26:28 +0200

    Add: partition-equal-subset-sum - submission-4

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-4.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    bool dfs(int i, int cur, int target, vector<int>& nums){
+        if(cur == target) return true;
+        if(cur > target) return false;
+
+        for(int j = i+1; j < nums.size(); ++j){
+            cur += nums[j];
+            if(dfs(j, cur, target, nums)) return true;
+            cur -= nums[j];
+        }
+        return false;
+    }
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if(sum % 2) return false;
+        
+        return dfs(0, nums[0], sum / 2, nums);
+    }
+};
