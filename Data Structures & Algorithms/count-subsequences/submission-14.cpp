a86fc591821550f7c4f8b4a70928cbe5877bb7c5
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-07-16 21:33:47 +0200

    Add: count-subsequences - submission-14

---
+++ Data Structures & Algorithms/count-subsequences/submission-14.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int m = s.size(), n = t.size();
+        vector<int> dp(n + 1, 0);
+        dp[n] = 1;
+
+        for(int i = m - 1; i >= 0; --i){
+            int prev = 1;
+            for(int j = n - 1; j >= 0; --j){
+                int tmp = dp[j];
+                if(s[i] == t[j]) dp[j] += prev;
+                prev = tmp;;
+            }
+        }
+        
+        return dp[0];
+    }
+};
