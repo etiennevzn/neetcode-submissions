76471f427853230715fb35dcf88a551f2b87f916
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-07-16 13:20:07 +0200

    Add: longest-increasing-path-in-matrix - submission-3

---
+++ Data Structures & Algorithms/longest-increasing-path-in-matrix/submission-3.cpp
@@ -0,0 +1,38 @@
+class Solution {
+private:
+    vector<pair<int,int>> directions{{1,0}, {-1,0}, {0,1}, {0,-1}};
+    int ROWS, COLS;
+    vector<vector<int>> memo;
+
+    int dfs(int r, int c, vector<vector<int>>& matrix){
+        if(r < 0 || r >= ROWS || c < 0 || c >= COLS || matrix[r][c] == -1) return 0;
+        if(memo[r][c] != -1) return memo[r][c];
+
+        memo[r][c] = 1;
+        int maxPath = 0;
+        for(const auto& dir : directions){
+            int newr = r + dir.first, newc = c + dir.second;
+            if(newr < 0 || newr >= ROWS || newc < 0 || newc >= COLS || matrix[newr][newc] == -1) continue;
+            if(matrix[newr][newc] > matrix[r][c]){
+                maxPath = max(maxPath, dfs(newr, newc, matrix));
+            }
+        }
+        memo[r][c] += maxPath;
+        return memo[r][c];
+    }
+public:
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        ROWS = matrix.size();
+        COLS = matrix[0].size();
+        memo.resize(ROWS, vector<int>(COLS, -1));
+        int res = 0;
+
+        for(int i = 0; i < ROWS; ++i){
+            for(int j = 0; j < COLS; j++){
+                res = max(res, dfs(i, j, matrix));
+            }
+        }
+        
+        return res;
+    }
+};
