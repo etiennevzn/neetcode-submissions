0926652ece8d30e715938b2232e097e6a447fa2d
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-07-17 18:05:07 +0200

    Add: jump-game-ii - submission-5

---
+++ Data Structures & Algorithms/jump-game-ii/submission-5.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    vector<int> memo;
+    int dfs(int i, vector<int>& nums){
+        if(i == nums.size() - 1) return 0;
+        if(memo[i] != -1) return memo[i];
+
+        int res = nums.size();
+        for(int j = 1; j <= nums[i]; ++j){
+            if(i + j < nums.size()) res = min(res, 1 + dfs(i + j, nums));
+        }
+        memo[i] = res;
+        return res;
+    }
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return 0;
+        memo.resize(n, -1);
+        return dfs(0, nums);
+    }
+};
