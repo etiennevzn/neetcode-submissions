03a5ed98c6c8d3de1d81ee6ce089ff1dea10cec6
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-07-19 00:03:10 +0200

    Add: non-cyclical-number - submission-5

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-5.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        int slow = n, fast = sumOfSquare(n);
+        while(slow != fast){
+            slow = sumOfSquare(slow);
+            fast = sumOfSquare(sumOfSquare(fast));
+        }
+        return slow == 1;
+    }
+
+    int sumOfSquare(int n){
+        int output = 0;
+        while(n > 0){
+            output += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        return output;
+    }
+};
