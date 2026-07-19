4e3395eb49f2656e67c664e801491b642c34a32d
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-07-19 19:06:22 +0200

    Add: reverse-integer - submission-2

---
+++ Data Structures & Algorithms/reverse-integer/submission-2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverse(int x) {
+        string s = to_string(x);
+        if(s[0] == '-'){
+            std::reverse(s.begin() + 1, s.end());
+        }else{
+            std::reverse(s.begin(), s.end());
+        }
+        long long res = stoll(s);
+        return res > INT_MAX || res < INT_MIN ? 0 : static_cast<int>(res);
+    }
+};
