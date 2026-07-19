ca32c35c5f13c224f178a091adabfcab848a6f3d
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-07-19 03:50:57 +0200

    Add: pow-x-n - submission-6

---
+++ Data Structures & Algorithms/pow-x-n/submission-6.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(x == 0) return 0;
+        if(n == 0) return 1;
+
+        int power = abs(n);
+        double res = 1;
+        while(power > 0){
+            if(power % 2){
+                res *= x;
+                power--;
+            }
+
+            x *= x;
+            power /=2;
+        }
+
+        return (n < 0) ? 1 / res : res;
+    }
+};
