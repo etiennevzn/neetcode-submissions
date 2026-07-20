709cf9e3fe39acadb75c99d7ad2a23a11cf19ae9
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-07-20 04:06:26 +0200

    Add: longest-common-prefix - submission-3

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-3.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string res = strs[0];
+        for(int i = 1; i < strs.size(); ++i){
+            int j = 0;
+            while(j < min(res.size(), strs[i].size())){
+                if(res[j] != strs[i][j]) break;
+                j++; 
+            }
+            res = res.substr(0, j);
+        }
+        return res;
+    }
+};
