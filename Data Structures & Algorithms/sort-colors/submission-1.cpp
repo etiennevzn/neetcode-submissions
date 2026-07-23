89364eeb8a55f0a6bb59d36f854f9f4cef219f17
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-07-23 10:14:38 +0200

    Add: sort-colors - submission-1

---
+++ Data Structures & Algorithms/sort-colors/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        unordered_map<int,int> count;
+        for(int num : nums){
+            count[num]++;
+        }
+
+        int k = 0;
+        for(int i = 0; i < 3; i++){
+            while(count[i]--){
+                nums[k++] = i;
+            }
+        }
+    }
+};
