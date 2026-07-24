2ff0c42f37fcb80fd3e0a3283fe1fc728899eefb
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-07-25 00:10:06 +0200

    Add: extra-characters-in-a-string - submission-3

---
+++ Data Structures & Algorithms/extra-characters-in-a-string/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minExtraChar(string s, vector<string>& dictionary) {
+        unordered_set<string> dic = unordered_set<string>(dictionary.begin(), dictionary.end());
+        int n = s.size();
+        vector<int> dp(n + 1, 0);
+
+        for(int i = n - 1; i >= 0; --i){
+            dp[i] = 1 + dp[i + 1];
+            for(int j = i; j < n; j++){
+                if(dic.count(s.substr(i, j - i + 1))){
+                    dp[i] = min(dp[i], dp[j + 1]);
+                }
+            }
+        }
+        return dp[0];
+    }
+};
