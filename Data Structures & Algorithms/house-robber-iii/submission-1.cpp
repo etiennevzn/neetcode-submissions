dd650a2d3add3da025ec86bb51e81cca2bde4c4b
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-07-26 00:38:02 +0200

    Add: house-robber-iii - submission-1

---
+++ Data Structures & Algorithms/house-robber-iii/submission-1.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    unordered_map<TreeNode*, int> memo;
+    int dfs(TreeNode* root){
+        if(!root) return 0;
+        if(memo.count(root)) return memo[root];
+        int rightRob = 0;
+        int leftRob = 0;
+        if(root->right) rightRob = dfs(root->right->right) + dfs(root->right->left);
+        if(root->left) leftRob = dfs(root->left->right) + dfs(root->left->left);
+
+        int rob = root->val + rightRob + leftRob;
+        int noRob = dfs(root->left) + dfs(root->right);
+        memo[root] = max(rob, noRob);
+        return max(rob, noRob);
+    }
+public:
+    int rob(TreeNode* root) {
+        return dfs(root);
+    }
+};
