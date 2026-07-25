5c83fb464cac8ab0e3feba6220749a764c842947
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-07-25 16:04:26 +0200

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int res = 0, l = 0, r = people.size() - 1;
+        sort(people.begin(), people.end());
+
+        while(l < r){
+            res++;
+            if(people[l] + people[r] <= limit){
+                l++;
+                r--;
+            }else{
+                r--;
+            }
+        }
+
+        return l == r ? res + 1 : res;
+    }
+};
