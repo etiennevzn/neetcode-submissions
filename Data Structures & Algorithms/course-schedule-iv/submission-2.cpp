ac63bf7126504b2957c2f5d20baa6bf8a3a609da
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-07-28 01:38:16 +0200

    Add: course-schedule-iv - submission-2

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-2.cpp
@@ -0,0 +1,33 @@
+class Solution {
+private:
+    vector<vector<int>> memo;
+    bool dfs(int u, int v, const unordered_map<int,unordered_set<int>>& dep){
+        if(memo[u][v] != -1) return memo[u][v];
+
+        auto it = dep.find(u);
+        if(it == dep.end()) return memo[u][v] = false;
+        if(dep.at(u).count(v)) return memo[u][v] = true;
+        for(int d : dep.at(u)){
+            if(dfs(d, v, dep)) return memo[u][v] = true;
+        }
+        return memo[u][v] = false;
+    }
+public:
+    vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
+        unordered_map<int,unordered_set<int>> dep;
+        for(const auto& pre : prerequisites){
+            dep[pre[0]].insert(pre[1]);
+        }
+
+        memo.resize(numCourses + 1, vector<int>(numCourses + 1, -1));
+        int n = queries.size();
+        vector<bool> res(n, false);
+        for(int i = 0; i < n; ++i){
+            vector<int> curr = queries[i];
+            int u = curr[0], v = curr[1];
+            res[i] = dfs(u, v, dep);
+        }
+
+        return res;
+    }
+};
