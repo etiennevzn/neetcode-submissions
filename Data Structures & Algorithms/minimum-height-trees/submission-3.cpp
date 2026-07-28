32eb777ededb6f70e0fcb4ee37d5c17c69ff80b6
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-07-28 23:56:13 +0200

    Add: minimum-height-trees - submission-3

---
+++ Data Structures & Algorithms/minimum-height-trees/submission-3.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        unordered_map<int, vector<int>> graph;
+        for(const auto& edge : edges){
+            int a = edge[0], b = edge[1];
+            graph[a].push_back(b);
+            graph[b].push_back(a);
+        }
+
+        vector<int> res;
+        int minHeight = n;
+        int curHeight = 0;
+        unordered_set<int> visited;
+        queue<int> q;
+
+        for(int root = 0; root < n; ++root){
+            q = {};
+            visited = {};
+            curHeight = 0;
+            q.push(root);
+            visited.insert(root);
+            while(!q.empty()){
+                int qSize = q.size();
+                for(int i = 0; i < qSize; ++i){
+                    int curr = q.front();
+                    q.pop();
+                    if(!graph.count(curr)) continue;
+                    for(int nei : graph[curr]){
+                        if(visited.count(nei)) continue;
+                        visited.insert(nei);
+                        q.push(nei);
+                    }
+                }
+                curHeight++;
+            }
+
+            if(curHeight == minHeight){
+                res.push_back(root);
+            }else if(curHeight < minHeight){
+                res = {root};
+                minHeight = curHeight;
+            }
+        }
+
+        return res;
+    }
+};
