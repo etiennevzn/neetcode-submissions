27ebe411d917bdb7a5d7074a06480cc5d02bd6ae
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-08-06 22:55:05 +0200

    Add: reorganize-string - submission-4

---
+++ Data Structures & Algorithms/reorganize-string/submission-4.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        unordered_map<char, int> freq;
+        int n = s.size();
+        for(const char& c : s){
+            freq[c]++;
+            if(freq[c] > (n + 1) / 2) return "";
+        }
+        
+        priority_queue<pair<int,char>> maxHeap;
+
+        for(const auto& p : freq){
+            maxHeap.push({p.second, p.first});
+        }
+        
+        string res = "";
+        while(!maxHeap.empty()){
+            auto [freq, c] = maxHeap.top();
+            maxHeap.pop();
+            if(res.empty() || c != res[res.size() - 1]){
+                freq--;
+                res.push_back(c);
+                if(freq > 0) maxHeap.push({freq, c});
+            }else if(!maxHeap.empty()){
+                auto [freq2, c2] = maxHeap.top();
+                maxHeap.pop();
+                freq2--;
+                res.push_back(c2);
+                if(freq2 > 0) maxHeap.push({freq2, c2});
+                maxHeap.push({freq, c});
+            }
+        }
+
+        return res;
+    }
+};
