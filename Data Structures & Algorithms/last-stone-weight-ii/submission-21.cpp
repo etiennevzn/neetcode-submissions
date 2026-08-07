3f7bfb9424db28e6cd3bda65043b355c255a2481
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-08-08 00:11:26 +0200

    Add: last-stone-weight-ii - submission-21

---
+++ Data Structures & Algorithms/last-stone-weight-ii/submission-21.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int lastStoneWeightII(vector<int>& stones) {
+        int stoneSum = accumulate(stones.begin(), stones.end(), 0);
+        int target = stoneSum / 2;
+        unordered_set<int> dp = {0};
+
+        for(int stone : stones){
+            unordered_set<int> newDp = dp;
+            for(int sum : dp){
+                if(sum + stone == target) return stoneSum - 2 * target;
+                if(sum + stone < target) newDp.insert(sum + stone);
+            }
+            dp = newDp;
+        }
+
+        int maxVal = 0;
+        for(int sum : dp){
+            maxVal = max(maxVal, sum);
+        }
+
+        return stoneSum - 2 * maxVal;
+    }
+};
