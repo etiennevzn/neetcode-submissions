894a6adaf975db291d532dce63f229d961ccd538
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-08-07 19:57:18 +0200

    Add: last-stone-weight-ii - submission-12

---
+++ Data Structures & Algorithms/last-stone-weight-ii/submission-12.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lastStoneWeightII(vector<int>& stones) {
+        int stoneSum = accumulate(stones.begin(), stones.end(), 0);
+        int target = stoneSum / 2;
+        vector<int> line(stoneSum + 1, 0);  
+        for(int i = 0; i <= stoneSum; ++i){
+            line[i] = abs(i - (stoneSum - i));
+        }
+
+        for(int i = stones.size() - 1; i >= 0; --i){
+            vector<int> newLine(stoneSum + 1, 0);
+            for(int j = 0; j <= stoneSum; ++j){
+                if(j + stones[i] <= stoneSum) newLine[j] = min(line[j], line[j + stones[i]]);
+                else break;
+            }
+            line = newLine;
+        }
+
+        return line[0];
+    }
+};
