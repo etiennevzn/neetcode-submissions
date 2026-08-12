6fb80ad2df1436e9ddf3f7c67a13cd121f602a59
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-08-12 15:21:33 +0200

    Add: dota2-senate - submission-6

---
+++ Data Structures & Algorithms/dota2-senate/submission-6.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        queue<int> direIdx;
+        queue<int> radiantIdx;
+        int n = senate.size();
+
+        for(int i = 0; i < n; ++i){
+            char c = senate[i];
+            if(c == 'R'){
+                radiantIdx.push(i);
+            }else{
+                direIdx.push(i);
+            }
+        }
+
+        while(!direIdx.empty() && !radiantIdx.empty()){
+            int dire = direIdx.front();
+            direIdx.pop();
+            int radiant = radiantIdx.front();
+            radiantIdx.pop();
+
+            if(dire < radiant){
+                direIdx.push(dire + n);
+            }else{
+                radiantIdx.push(radiant + n);
+            }
+        }
+
+        return direIdx.empty() ? "Radiant" : "Dire";
+    }
+};
