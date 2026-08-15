e3225941045cdc1bd9b8a69139353e0dade56d7f
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-08-15 22:30:40 +0200

    Add: minimum-array-end - submission-4

---
+++ Data Structures & Algorithms/minimum-array-end/submission-4.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    long long minEnd(int n, int x) {
+        long long res = 0;
+        n -= 1;
+
+        vector<int> x_bin(32, 0);
+        vector<int> n_bin(32, 0);
+
+        for(int i = 0; i < 32; ++i){
+            x_bin[i] = (x >> i) & 1;
+            n_bin[i] = (n >> i) & 1;
+        }
+
+        int i = 0, j = 0;
+        while(i < x_bin.size() && j < n_bin.size()){
+            if(x_bin[i]){
+                i++;
+                continue;
+            }
+
+            x_bin[i++] = n_bin[j++];
+        }
+
+        while(j < n_bin.size()) x_bin.push_back(n_bin[j++]);
+
+        for(int i = 0; i < x_bin.size(); ++i){
+            res |= (long long)x_bin[i] << i;
+        }
+
+        return res;
+    }
+};
