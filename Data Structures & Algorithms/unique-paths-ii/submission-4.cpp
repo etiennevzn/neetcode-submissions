e1824eec5c943b8b432705929cdb36985839ec5f
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-08-18 15:11:26 +0200

    Add: unique-paths-ii - submission-4

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-4.cpp
@@ -0,0 +1,21 @@
+class Solution {
+private:
+    int ROWS, COLS;
+    vector<vector<int>> memo;
+    int dfs(int r, int c, vector<vector<int>>& grid){
+        if(r == ROWS - 1 && c == COLS - 1) return 1;
+        if(r == ROWS || c == COLS || grid[r][c] == 1) return 0;
+
+        if(memo[r][c] != -1) return memo[r][c];
+        return memo[r][c] = dfs(r + 1, c, grid) + dfs(r, c + 1, grid);
+    }
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        ROWS = obstacleGrid.size();
+        COLS = obstacleGrid[0].size();
+        if(ROWS == COLS && ROWS == 1) return obstacleGrid[0][0] == 1 ? 0 : 1;
+
+        memo.resize(ROWS, vector<int>(COLS, -1));
+        return dfs(0, 0, obstacleGrid);
+    }
+};
