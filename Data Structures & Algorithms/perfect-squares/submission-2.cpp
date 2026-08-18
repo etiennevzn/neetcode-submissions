16c2ea079960702dd2ed53137d12c29dd3f22b6f
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-08-18 16:33:12 +0200

    Add: perfect-squares - submission-2

---
+++ Data Structures & Algorithms/perfect-squares/submission-2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    vector<int> memo;
+    int dfs(int target, int limit){
+        if(target == 0) return 0;
+        if(target < 0) return limit;
+
+        if(memo[target] != -1) return memo[target];
+
+        int res = INT_MAX;
+        for(int i = 1; i < limit; ++i){
+            res = min(res, 1 + dfs(target - (i * i), limit));
+        }
+        return memo[target] = res;
+    }
+public:
+    int numSquares(int n) {
+        int i = 1;
+        while(true){
+            if(i * i == n) return 1;
+            if(i * i > n) break;
+            i++;
+        }
+
+        memo.resize(n + 1, -1);
+        return dfs(n, i);
+    }
+};
