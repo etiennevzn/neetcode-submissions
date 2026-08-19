613661c329fa9e89a1c4714488282b84a4b81a88
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-08-19 20:56:59 +0200

    Add: jump-game-vii - submission-4

---
+++ Data Structures & Algorithms/jump-game-vii/submission-4.cpp
@@ -0,0 +1,23 @@
+class Solution {
+private:
+    vector<int> memo;
+
+    bool dfs(int i, const string& s, int minJump, int maxJump){
+        if(s[i] == '1') return memo[i] = false;
+        if(i == s.size() - 1) return true;
+        if(memo[i] != -1) return memo[i];
+
+        for(int jump = maxJump; jump >= minJump; --jump){
+            int arrival = i + jump;
+            if(arrival > s.size() - 1) continue;
+            if(dfs(arrival, s, minJump, maxJump)) return memo[i] = true;
+        }
+        
+        return memo[i] = false;
+    }
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        memo.resize(s.size(), -1);
+        return dfs(0, s, minJump, maxJump);
+    }
+};
