7f7c063bf0b5f4772ab1a83ffe7c83d6e38668fa
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-08-22 14:23:41 +0200

    Add: remove-duplicates-from-sorted-array - submission-2

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int idx = 0;
+
+        for(int i = 0; i < nums.size(); ++i){
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            nums[idx++] = nums[i];
+        }
+
+        return idx;
+    }
+};
