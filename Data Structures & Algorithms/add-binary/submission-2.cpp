253e712abed825e00061fd3ef9dc95645e333e08
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-08-24 00:19:19 +0200

    Add: add-binary - submission-2

---
+++ Data Structures & Algorithms/add-binary/submission-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int carry = 0, i = a.size() - 1, j = b.size() - 1;
+        string res = "";
+
+        while(i >= 0 || j >= 0 || carry > 0){
+            int digitA = i >= 0 ? a[i] - '0' : 0;
+            int digitB = j >= 0 ? b[j] - '0' : 0;
+
+            int total = digitA + digitB + carry;
+            res += (total % 2 ) + '0';
+            carry = total / 2;
+            i--;
+            j--;
+        }
+
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
