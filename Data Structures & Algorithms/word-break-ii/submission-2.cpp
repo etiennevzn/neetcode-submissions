b2b9c08f9f60e4be9ab40e838a7db3e2451404c2
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-08-27 15:35:51 +0200

    Add: word-break-ii - submission-2

---
+++ Data Structures & Algorithms/word-break-ii/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    vector<string> res;
+    void dfs(int i, string curWord, string cur, string s, unordered_set<string>& wordDict){
+        if(i == s.size()){
+            if(wordDict.count(curWord)){
+                cur += curWord;
+                res.push_back(cur);
+            }        
+            return;
+        }
+
+        if(wordDict.count(curWord)){
+            dfs(i, "", cur + curWord + " ", s, wordDict);
+        }
+
+        dfs(i + 1, curWord + s[i], cur, s, wordDict);
+    }
+public:
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+        dfs(0, "", "", s, dict);
+        return res;
+    }
+};
