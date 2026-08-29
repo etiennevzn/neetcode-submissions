cc89d5688d1a50d4905b3559b97dd90cf22e958e
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-08-29 07:28:15 +0200

    Add: greatest-common-divisor-of-strings - submission-1

---
+++ Data Structures & Algorithms/greatest-common-divisor-of-strings/submission-1.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if(str1.size() > str2.size()) swap(str1, str2);
+
+        string res = str1;
+
+        while(!res.empty()){
+            if(str1.size() % res.size() || str2.size() % res.size()){
+                res.pop_back();
+                continue;
+            }
+
+            int div1 = str1.size() / res.size();
+            int div2 = str2.size() / res.size();
+
+            string compare = "";
+            bool equal1 = false, equal2 = false;
+            for(int i = 0; i < div1; ++i){
+                compare += res;
+            }
+            if(compare == str1) equal1 = true;
+
+            compare = "";
+            for(int i = 0; i < div2; ++i){
+                compare += res;
+            }
+            if(compare == str2) equal2 = true;
+
+            if(equal1 && equal2) return res;
+            res.pop_back();
+        }
+
+        return "";
+    }
+};
