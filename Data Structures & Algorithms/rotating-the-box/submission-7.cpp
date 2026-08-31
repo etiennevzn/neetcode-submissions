917fbb3dc8997fd0441cb7dbc6f15b72a380b99f
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-08-31 16:31:24 +0200

    Add: rotating-the-box - submission-7

---
+++ Data Structures & Algorithms/rotating-the-box/submission-7.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& boxGrid) {
+        int n = boxGrid.size(), m = boxGrid[0].size();
+        vector<vector<char>> res(m, vector<char>(n, '.'));
+
+        for(int i = 0; i < n; ++i){
+            int slot = m - 1;
+            for(int j = m - 1; j >= 0; --j){
+                if(boxGrid[i][j] == '#'){
+                    res[slot--][n - i - 1] = '#';
+                }else if(boxGrid[i][j] == '*'){
+                    res[j][n - i - 1] = '*';
+                    slot = j - 1;
+                }
+            }
+        }
+
+        return res;
+    }
+};
