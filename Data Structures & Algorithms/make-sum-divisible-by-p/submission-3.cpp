76162f81dde24882b557cb969c4ef1b5e231ca03
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-09-04 12:29:18 +0200

    Add: make-sum-divisible-by-p - submission-3

---
+++ Data Structures & Algorithms/make-sum-divisible-by-p/submission-3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minSubarray(vector<int>& nums, int p) {
+        long long sum = accumulate(nums.begin(), nums.end(), 0);
+        if(!(sum % p)) return 0;
+
+        int n = nums.size();
+        for(int res = 1; res < n; ++res){
+            long long curSum = 0;
+            for(int i = 0; i < n; ++i){
+                curSum += nums[i];
+                if(i >= res) curSum -= nums[i - res];
+
+                if((sum - curSum) % p == 0) return res;
+            }
+        }
+
+        return -1;
+    }
+};
