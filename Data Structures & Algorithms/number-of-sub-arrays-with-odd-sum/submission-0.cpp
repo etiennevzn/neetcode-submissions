6bca12ed8632066ba756f4d738f6174c92c89214
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-09-06 16:13:08 +0200

    Add: number-of-sub-arrays-with-odd-sum - submission-0

---
+++ Data Structures & Algorithms/number-of-sub-arrays-with-odd-sum/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numOfSubarrays(vector<int>& arr) {
+        int n = arr.size();
+        int res = 0;
+        for(int i = 0; i < n; ++i){
+            long long subSum = 0;
+            for(int j = i; j < n; ++j){
+                subSum += arr[j];
+                if(subSum % 2) res++;
+            }
+        }
+
+        return res;
+    }
+};
