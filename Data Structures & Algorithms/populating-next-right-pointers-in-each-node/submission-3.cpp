09fd3f14641197f61d8d37dc94585fb2a3055d2a
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-09-07 10:08:45 +0200

    Add: populating-next-right-pointers-in-each-node - submission-3

---
+++ Data Structures & Algorithms/populating-next-right-pointers-in-each-node/submission-3.cpp
@@ -0,0 +1,39 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+private:
+
+public:
+    Node* connect(Node* root) {
+        if(!root) return root;
+
+        Node* cur = root, *nxt = root->left; 
+        while(nxt){
+            cur->left->next = cur->right;
+            if(cur->next) cur->right->next = cur->next->left;
+            cur = cur->next;
+            if(!cur){
+                cur = nxt;
+                nxt = cur->left;
+            }
+        }
+
+        return root;
+    }
+};
