2cbc0d02406f3952fbbd61f052aa1286af261737
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-09-08 16:13:10 +0200

    Add: lowest-common-ancestor-of-a-binary-tree - submission-1

---
+++ Data Structures & Algorithms/lowest-common-ancestor-of-a-binary-tree/submission-1.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    vector<TreeNode*> ancestors;
+    bool dfs(TreeNode* root, TreeNode* node){
+        ancestors.push_back(root);
+
+        if(root == node) return true;
+        if(root->left && dfs(root->left, node)) return true;
+        if(root->right && dfs(root->right, node)) return true;
+        
+        ancestors.pop_back();
+        return false;
+    }
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        ancestors.clear();
+        dfs(root, p);
+        vector<TreeNode*> pAncestors = ancestors;
+        ancestors.clear();
+        dfs(root, q);
+        vector<TreeNode*> qAncestors = ancestors;
+
+        for(int i = pAncestors.size() - 1; i >= 0; --i){
+            if(find(qAncestors.begin(), qAncestors.end(), pAncestors[i]) != qAncestors.end()) return pAncestors[i];
+        }
+
+        return root;
+    }
+};
