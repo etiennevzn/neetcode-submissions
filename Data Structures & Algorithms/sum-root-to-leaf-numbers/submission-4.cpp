68b2a4b72d799621f3793739972474d6fd091afc
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-09-10 15:47:28 +0200

    Add: sum-root-to-leaf-numbers - submission-4

---
+++ Data Structures & Algorithms/sum-root-to-leaf-numbers/submission-4.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int dfs(TreeNode* root, int cur){
+        if(!root) return 0;
+
+        cur *= 10;
+        cur += root->val;
+        if(!root->left && !root->right){
+            return cur;
+        }
+
+        return dfs(root->left, cur) + dfs(root->right, cur);
+    }
+
+public:
+    int sumNumbers(TreeNode* root) {
+        return dfs(root, 0);
+    }
+};
