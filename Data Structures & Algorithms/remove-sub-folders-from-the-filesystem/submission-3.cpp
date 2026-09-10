83b32c41664c8a3f0779b7db6dc150c2f76085b0
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-09-10 17:58:52 +0200

    Add: remove-sub-folders-from-the-filesystem - submission-3

---
+++ Data Structures & Algorithms/remove-sub-folders-from-the-filesystem/submission-3.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<string> removeSubfolders(vector<string>& folder) {
+        vector<string> res;
+        sort(folder.begin(), folder.end());
+        res.push_back(folder[0]);
+
+        for(int i = 1; i < folder.size(); ++i){
+            if(folder[i].find(res.back() + "/") != 0) res.push_back(folder[i]);
+        }
+
+        return res;
+    }
+};
