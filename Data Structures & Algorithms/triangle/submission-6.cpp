25d1ad032a286d02516aafdcce6edb8d4d6b72d3
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-09-11 15:31:44 +0200

    Add: triangle - submission-6

---
+++ Data Structures & Algorithms/triangle/submission-6.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> memo;
+    int dfs(int i, int j, const vector<vector<int>>& triangle){
+        if(i == triangle.size()) return 0;
+        if(memo[i][j] != INT_MAX) return memo[i][j];
+
+        return memo[i][j] = triangle[i][j] + min(dfs(i + 1, j, triangle), dfs(i + 1, j + 1, triangle));
+    }
+
+    int minimumTotal(vector<vector<int>>& triangle) {
+        memo.resize(triangle.size());
+        for (int i = 0; i < triangle.size(); ++i) memo[i].resize(triangle[i].size(), INT_MAX);
+
+        return dfs(0, 0, triangle);
+    }
+};
