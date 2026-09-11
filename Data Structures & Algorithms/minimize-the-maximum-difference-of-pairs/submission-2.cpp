5b8759c4852a24f476e33d59e2a7c4e043ecb01c
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-09-11 13:20:49 +0200

    Add: minimize-the-maximum-difference-of-pairs - submission-2

---
+++ Data Structures & Algorithms/minimize-the-maximum-difference-of-pairs/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+private:    
+    vector<vector<int>> memo;
+    int dfs(int i, int p, const vector<int>& nums){
+        if(p == 0) return 0;
+        if(i >= nums.size() - 1) return INT_MAX;
+        if(memo[i][p] != -1) return memo[i][p];
+
+        int take = max(abs(nums[i] - nums[i + 1]), dfs(i + 2, p - 1, nums));
+        int skip = dfs(i + 1, p, nums);
+
+        return memo[i][p] = min(take, skip);
+    }
+public:
+    int minimizeMax(vector<int>& nums, int p) {
+        memo.resize(nums.size(), vector<int>(p + 1, -1));
+        sort(nums.begin(), nums.end());
+        return dfs(0, p, nums);
+    }
+};
