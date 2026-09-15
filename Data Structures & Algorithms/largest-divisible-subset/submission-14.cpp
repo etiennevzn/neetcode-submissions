64c94577d74656a293a99d61b2e0dcde1142f308
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-09-15 08:26:49 +0200

    Add: largest-divisible-subset - submission-14

---
+++ Data Structures & Algorithms/largest-divisible-subset/submission-14.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+
+        vector<vector<int>> dp(n);
+        vector<int> final = dp[n - 1];
+
+        for(int i = n - 1; i >= 0; --i){
+            vector<int> res = {nums[i]};
+            for(int j = i + 1; j < nums.size(); ++j){
+                if(nums[j] % nums[i] == 0){
+                    vector<int> tmp = dp[j];
+                    tmp.insert(tmp.begin(), nums[i]);
+                    if(tmp.size() > res.size()) res = tmp;
+                }
+            }
+            dp[i] = res;
+            if(dp[i].size() > final.size()) final = dp[i];
+        }
+
+        return final;
+    }
+};
