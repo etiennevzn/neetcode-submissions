1eac80d833d936dbcccd6cf68d5b8f2133c4b727
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-09-24 19:34:05 +0200

    Add: is-subsequence - submission-3

---
+++ Data Structures & Algorithms/is-subsequence/submission-3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int n = s.size(), m = t.size();
+        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+        for(int j = 0; j < m + 1; ++j) dp[n][j] = true;
+
+        for(int i = n - 1; i >= 0; --i){
+            for(int j = m - 1; j >= i; --j){
+                if(s[i] == t[j]){
+                    dp[i][j] = dp[i + 1][j + 1];
+                }else{
+                    dp[i][j] = dp[i][j + 1];
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+};
