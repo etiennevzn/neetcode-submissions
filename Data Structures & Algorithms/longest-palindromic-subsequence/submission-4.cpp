fcc8f0bf2fbbca81e004284ed7e863bd260af306
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-09-24 15:05:44 +0200

    Add: longest-palindromic-subsequence - submission-4

---
+++ Data Structures & Algorithms/longest-palindromic-subsequence/submission-4.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    vector<vector<int>> memo;
+    int dfs(int l, int r, const string& s){
+        if(l > r) return 0;
+        if(l == r) return 1;
+        if(memo[l][r] != -1) return memo[l][r];
+
+        int res = 0;
+        if(s[l] == s[r]){
+            res = 2 + dfs(l + 1, r - 1, s);
+        }else{
+            res = max(dfs(l + 1, r, s), dfs(l, r - 1, s));
+        }
+
+        return memo[l][r] = res;
+    }
+public:
+    int longestPalindromeSubseq(string s) {
+        int n = s.size();
+        memo.resize(n, vector<int>(n, -1));
+        return dfs(0, s.size() - 1, s);
+    }
+};
