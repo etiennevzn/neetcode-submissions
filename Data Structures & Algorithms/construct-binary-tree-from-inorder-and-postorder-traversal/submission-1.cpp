37eaaadfa8e2df090f2223e9b58642ab32c8cea0
Etienne VEZIEN <166559256+etiennevzn@users.noreply.github.com>
2026-09-26 15:26:38 +0200

    Add: construct-binary-tree-from-inorder-and-postorder-traversal - submission-1

---
+++ Data Structures & Algorithms/construct-binary-tree-from-inorder-and-postorder-traversal/submission-1.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    TreeNode* build(int iStart, int iEnd, int& pIdx, const vector<int>& inorder, const vector<int>& postorder, unordered_map<int,int>& inIdx){
+        if(iStart > iEnd) return nullptr;
+        TreeNode* root = new TreeNode(postorder[pIdx--]);
+        if(iStart == iEnd) return root;
+
+        int j = inIdx[root->val];
+        root->right = build(j + 1, iEnd, pIdx, inorder, postorder, inIdx); 
+        root->left = build(iStart, j - 1, pIdx, inorder, postorder, inIdx);
+
+        return root;
+    }
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        unordered_map<int,int> inIdx;
+        for(int i = 0; i < inorder.size(); ++i){
+            inIdx[inorder[i]] = i;
+        }
+        int iEnd = inorder.size() - 1, pIdx = postorder.size() - 1;
+        return build(0, iEnd, pIdx, inorder, postorder, inIdx);
+    }
+};
